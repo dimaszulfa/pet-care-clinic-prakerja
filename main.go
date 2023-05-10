8634358e74f7904db4db5e50ea420c2a94c0b194
Dimas Zulfa Santana <dimaszulfa0@gmail.com>
2023-05-10 14:52:10 +0700

    trying connect to postgres database

--- main.go
+++ main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+
 	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
 )
 
 type BaseResponse struct {
-	Status  bool        `json:true`
-	Message string      `json:message`
-	Data    interface{} `json:data`
+	Status  bool        `json:"true"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
 }
 
 func main() {
+	// Connection parameters
+	connStr := "user=postgres password=admin123 dbname=db_prakerja sslmode=disable"
+
+	// Open the connection
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Test the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to the PostgreSQL database!")
+
+	// Perform database operations...
 	e := echo.New()
 	e.GET("/pets", GetPetsController)
 	e.Start(":8000")
